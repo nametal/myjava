83d34a12c671524c2337ca27bea65e52106af24e
HarunA <spinfusor.gt@gmail.com>
2014-09-13 15:04:19 +0700

    add up to no.7

--- src/number/Euler.java
+++ src/number/Euler.java
@@ -107,6 +107,70 @@ public class Euler {
 		}
 		return ori==rev;
 	}
+	/* End of 4 */
+	
+	/* 5. Smallest multiple */
+	public static int smallestMul(int n) {
+		int a = n;
+		int b = 2;
+		while (b<n) {
+			b = 2;
+			while (b <= n) {
+				if(a%b!=0) break;
+				b++;
+			}
+			a++;
+		}
+		return a-1;
+	}
+	/* End of 5 */
+	
+	/* 6. Sum square difference */
+	// my solution
+	public static int sumSquareDiff(int n) {
+		int sumOfSq = 0;
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sumOfSq += i*i;
+			sum += i;
+		}
+		return sum*sum - sumOfSq;
+	}
+	
+	// efficient way
+	public static int sumSquareDiff_f(int n) {
+		int sumOfSq = (2*n+1)*(n+1)*n/6;
+		int sum = n*(n+1)/2;
+		return sum*sum - sumOfSq;
+	}
+	/* End of 6 */
+	
+	/* 7. 10 001st prime */
+	public static boolean isPrime(int n) {
+//		System.out.printf("%d=",n);
+		if(n == 2) return true; // the only even prime
+		if(n < 2 || n % 2 == 0) return false;
+		int upper = (int) Math.sqrt(n) + 1;
+		for (int i = 3; i < upper; i+=2) {
+			if(n % i == 0) return false;
+		}
+		return true;
+	}
+	
+	public static int primeAt(int n) {
+		if(n == 1) return 2;
+		int c = 1;
+		int x = 1;
+		while(c < n) {
+			x += 2;
+			if (isPrime(x)) {
+				c++;
+//				System.out.println(c+" -> "+x);
+			}
+		}
+		return x;
+	}
+	/* End of 7 */
 	
 	public static void main(String[] args) {
 		/*int n=1000;
@@ -116,6 +180,10 @@ public class Euler {
 //		System.out.println(largestPrimeFactor2(600851475143L));
 //		System.out.println(isPalindromeNum(9009));
 //		System.out.println(isPalindromeNum_f(9009));
-		System.out.println(largestPalindromeProduct(3));
+//		System.out.println(largestPalindromeProduct(3));
+//		System.out.println(smallestMul(11)); // should be 27720
+//		System.out.println(smallestMul(9));
+//		System.out.println(sumSquareDiff(100));
+		System.out.println(primeAt(10001));
 	}
 }
