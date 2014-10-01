f65d1505687ceff4d73ae1353b1952048e455282
HarunA <spinfusor.gt@gmail.com>
2014-10-01 07:16:22 +0700

    refine my pow

--- src/number/MyPow.java
+++ src/number/MyPow.java
@@ -1,30 +1,36 @@
 package number;
 
+import java.math.BigDecimal;
+
 public class MyPow {
-	private static double IMAGINARY_NUMBER = 11.11;	// square root of negatives
-	private static double UNDEFINED = 22.22;		// zero to the zero
-	private static double INFINITY = 33.33;			// divide by zero
-	
 	public static double pow(double a, double b) {
 		if(b==0) {
-			if(a==0) return UNDEFINED;
+//			if(a==0)
+//				return UNDEFINED;
 			return 1;
 		}
 		if(a==0) {
-			if(b<0) return INFINITY;
+			if(b<0) 
+				return Double.POSITIVE_INFINITY;
 			return 0;
 		}
-		if(b%2==0) return pow(a,b/2) * pow(a,b/2);
+		if(b%2==0) 
+			return BigDecimal.valueOf(pow(a,b/2)).multiply(BigDecimal.valueOf(pow(a,b/2))).doubleValue();
 		else {
-			if(b<0) return 1/a*pow(a,b+1);
+			if(b%1!=0) { // fractional exponent
+				return Double.NaN; // TODO this1
+			}
+			if(b<0) 
+				return 1/a*pow(a,b+1);
 			return a*pow(a,b-1);
 		}
 	}
 	
 	public static void main(String[] args) {
-		double x = 5;
-		double y = -4;
-		System.out.println(Math.pow(x,y));
-		System.out.println(pow(x,y));
+		double[][] ds = {{0,0}, {0,1}, {0,-1}, {1,0}, {-1,0}, {-1,-1}, {3,1}, {2,3}, {-2,-3}, {-2,4}, {2,-2}};
+		for (int i = 0; i < ds.length; i++) {
+			System.out.printf("Math.pow(%4.1f,%4.1f) = %f\n",ds[i][0],ds[i][1],Math.pow(ds[i][0],ds[i][1]));
+			System.out.printf("  My pow(%4.1f,%4.1f) = %f\n",ds[i][0],ds[i][1],pow(ds[i][0],ds[i][1]));
+		}
 	}
 }
